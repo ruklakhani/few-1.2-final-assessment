f3623cb3aeaa15aa9ea78617bd72033cea65856b
ruk <ruk.lakhani@students.makeschool.com>
2021-03-03 16:09:16 -0800

    challenge 4 and 5 done

---
+++ src/MetalCard.js
@@ -0,0 +1,26 @@
+// src/MetalCard.js
+
+import React from 'react'
+import './MetalCard.css'
+
+function MetalCard(cards) {
+    const { band_name, fans, formed, origin, split, style} = cards
+    return (
+        <div className="metal-card">
+            <h1>{band_name}</h1>
+            <div>
+                fans: {fans} 
+                <br></br>
+                formed: {formed}
+                <br></br> 
+                origin: {origin} 
+                <br></br>
+                split: {split} 
+                <br></br>
+                styles: {style}
+                <br></br>
+            </div>
+        </div>
+    )
+}
+export default MetalCard
